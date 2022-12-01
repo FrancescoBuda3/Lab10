46f22bab32d0a5322e2926b06ce5172746df86d2
Francesco Buda <francesco.buda3@studio.unibo.it>
2022-12-01 17:04:01 +0100

    completato es 102, tranne l'ultimo metodo

--- 102-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 102-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -31,42 +31,47 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> orderedSongNames() {
-        return null;
+        return this.songs.stream().map(x -> x.getSongName()).sorted();
     }
 
     @Override
     public Stream<String> albumNames() {
-        return null;
+        return this.albums.keySet().stream();
     }
 
     @Override
     public Stream<String> albumInYear(final int year) {
-        return null;
+        return this.albumNames().filter(x -> this.albums.get(x).equals(year));
     }
 
     @Override
     public int countSongs(final String albumName) {
-        return -1;
+        return (int) this.songs.stream().filter(x -> x.getAlbumName().isPresent())
+                .filter(x -> x.getAlbumName().get().equals(albumName)).count();
     }
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int) this.songs.stream().filter(x -> x.getAlbumName().isEmpty()).count();
     }
 
     @Override
     public OptionalDouble averageDurationOfSongs(final String albumName) {
-        return null;
+        return this.songs.stream().filter(x -> x.getAlbumName().isPresent()).filter(x -> x.getAlbumName().get().equals(albumName)).mapToDouble(x -> x.getDuration()).average();
     }
 
     @Override
     public Optional<String> longestSong() {
-        return null;
+        return Optional.ofNullable(this.songs
+                .stream()
+                .max((x,y) -> x.getDuration() > y.getDuration() ? 1 : x.getDuration() == y.getDuration() ? 0 : -1)
+                .get()
+                .getSongName());
     }
 
     @Override
     public Optional<String> longestAlbum() {
-        return null;
+        return Optional.empty();
     }
 
     private static final class Song {
